bdcd9ba41d9bedb70bc6eadff5a079502837ad76
Saullo Bretas Silva <saullo.silva55@outlook.com>
2022-12-09 00:56:05 -0300

    St: Added Types.hpp file - System types

---
+++ St/Types.hpp
@@ -0,0 +1,31 @@
+/*
+ * Nashi Operating System
+ * Copyright (C) 2022 Saullo Bretas Silva
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+namespace St::Types
+{
+    using int8 = __INT8_TYPE__;
+    using uint8 = __UINT8_TYPE__;
+    using int16 = __INT16_TYPE__;
+    using uint16 = __UINT16_TYPE__;
+    using int32 = __INT32_TYPE__;
+    using uint32 = __UINT32_TYPE__;
+    using int64 = __INT64_TYPE__;
+    using uint64 = __UINT64_TYPE__;
+    using size = __SIZE_TYPE__;
+} // namespace St::Types
