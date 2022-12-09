f328ee62de1864f9f201688c8c3f6f82ca773d11
Saullo Bretas Silva <saullo.silva55@outlook.com>
2022-12-08 23:43:11 -0300

    Scripts: Added Run.sh file

---
+++ Scripts/Run.sh
@@ -0,0 +1,8 @@
+[ -z "$NASHIOS_QEMU_BIN" ] && NASHIOS_QEMU_BIN="qemu-system-i386"
+[ -z "$NASHIOS_QEMU_KERNEL" ] && NASHIOS_QEMU_KERNEL="Kernel/Kernel"
+[ -z "$NASHIOS_QEMU_ARGS" ] && NASHIOS_QEMU_ARGS="-kernel $NASHIOS_QEMU_KERNEL"
+
+[ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ] && NASHIOS_QEMU_VIRT="-enable-kvm"
+
+# shellcheck disable=SC2086
+$NASHIOS_QEMU_BIN $NASHIOS_QEMU_ARGS $NASHIOS_QEMU_VIRT
